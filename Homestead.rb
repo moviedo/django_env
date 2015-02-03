ddfd68dc33deb8d63cc3f63fb21d7dd7f695f7b3
moviedo <mauro.o.2006@gmail.com>
2015-02-03 11:53:22 -0500

    Added documentation for what happens in the configuration code. Removed php config.

--- Homestead.rb
+++ Homestead.rb
@@ -1,21 +1,35 @@
+# The most common configuration options are documented and commented below.
+# For a complete reference, please see the online documentation at
+# https://docs.vagrantup.com.
 class Homestead
+
+  # Configure The Box
   def Homestead.configure( config, settings )
-    # Configure The Box
+
+    # Every Vagrant development environment requires a box. You can search for
+    # boxes at https://atlas.hashicorp.com/search.
     config.vm.box = "ubuntu/trusty64"
     config.vm.hostname = "homestead"
 
-    # Configure A Private Network IP
+    # Create a private network, which allows host-only access to the machine
+    # using a specific IP.
     config.vm.network :private_network, ip: settings["ip"] ||= "192.168.10.10"
 
-    # Configure A Few VirtualBox Settings
+    # Provider-specific configuration so you can fine-tune various
+    # backing providers for Vagrant. These expose provider-specific options.
+    # View the documentation for the provider you are using for more
+    # information on available options.
     config.vm.provider "virtualbox" do |vb|
+      # Configure A Few VirtualBox Settings
       vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
       vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
       vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
       vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
     end
 
-    # Configure Port Forwarding To The Box
+    # Create a forwarded port mapping which allows access to a specific port
+    # within the machine from a port on the host machine. In the example below,
+    # accessing "localhost:8080" will access port 80 on the guest machine.
     config.vm.network "forwarded_port", guest: 80, host: 8000
     config.vm.network "forwarded_port", guest: 3306, host: 33060
     config.vm.network "forwarded_port", guest: 5432, host: 54320
@@ -36,36 +50,27 @@ class Homestead
     end
 
     # Copy The Bash Aliases
-    config.vm.provision "shell" do |s|
-      s.inline = "cp /vagrant/aliases /home/vagrant/.bash_aliases"
-    end
+    # config.vm.provision "shell" do |s|
+    #   s.inline = "cp /vagrant/aliases /home/vagrant/.bash_aliases"
+    # end
 
-    # Register All Of The Configured Shared Folders
+    # Share an additional folder to the guest VM. The first argument is
+    # the path on the host to the actual folder. The second argument is
+    # the path on the guest to mount the folder. And the optional third
+    # argument is a set of non-required options.
     settings["folders"].each do |folder|
+      # Register All Of The Configured Shared Folders
       config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
     end
 
-    # Install All The Configured Nginx Sites
-    settings["sites"].each do |site|
-      config.vm.provision "shell" do |s|
-          if (site.has_key?("hhvm") && site["hhvm"])
-            s.inline = "bash /vagrant/scripts/serve-hhvm.sh $1 $2"
-            s.args = [site["map"], site["to"]]
-          else
-            s.inline = "bash /vagrant/scripts/serve.sh $1 $2"
-            s.args = [site["map"], site["to"]]
-          end
-      end
+    # Enable provisioning with a shell script. Additional provisioners such as
+    # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
+    # documentation for more information about their specific syntax and use.
+
+    # Run provisioning script with asible
+    config.vm.provision "ansible" do |ansible|
+      ansible.playbook = "provisioning/ansible/playbook.yml"
     end
 
-    # Configure All Of The Server Environment Variables
-    if settings.has_key?("variables")
-      settings["variables"].each do |var|
-        config.vm.provision "shell" do |s|
-            s.inline = "echo \"\nenv[$1] = '$2'\" >> /etc/php5/fpm/php-fpm.conf && service php5-fpm restart"
-            s.args = [var["key"], var["value"]]
-        end
-      end
-    end
   end
 end
